6081e9958b6d9a891ed93c44d6d468f4b30fe0ce
Abbey Mondshein <5883999+abbeymondshein@users.noreply.github.com>
2020-08-25 11:08:08 -0400

    starter json file, loading entries, logging output

--- altar.js
+++ altar.js
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
 const { program } = require("commander");
 const fs = require("fs");
+const c = require("chalk");
+
+const FLAME = " ▴";
+const CANDLE = " ┃";
+const DIVIDER = "◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆";
+const SHELF = "┍━━━━━━━━━━━━━━━━┑";
 
 program
   .option("-l, --load", "Load Altar")
@@ -8,9 +14,9 @@ program
   .parse(process.argv);
 
 console.log(`
-%%%%%%%%%%%%%%%%
+${DIVIDER}
 Welcome to Altar
-%%%%%%%%%%%%%%%%
+${DIVIDER}
 
 for options & help, enter "altar -h"
 `);
@@ -24,5 +30,36 @@ fs.readFile("./altarItems.json", "utf8", (err, data) => {
     console.error(err);
     return;
   }
-  console.log(data);
+
+  // * Imports a sample set / starter via JSON
+  const candleEntries = JSON.parse(data);
+
+  // todo extract as util
+  const logFlames = (candleAmount) => {
+    console.log(`${c.yellow(FLAME.repeat(candleAmount))}`);
+  };
+
+  const logCandleWithColor = (candleEntries) => {
+    const combined = [];
+    candleEntries.forEach((element) => {
+      const { color } = element;
+      combined.push(`${c[color](CANDLE)}`);
+    });
+    console.log(combined.join(""));
+
+    return;
+  };
+
+  const logCandleIntentions = (candleEntries) => {
+    candleEntries.forEach((candleEntry) => {
+      const { color, intention } = candleEntry;
+      console.log(`${c[color](intention)}`);
+    });
+  };
+
+  // * LOG CANDLE OUTPUT
+  logFlames(candleEntries.length);
+  logCandleWithColor(candleEntries);
+  console.log(SHELF);
+  logCandleIntentions(candleEntries);
 });
