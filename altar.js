811a75da41c41bc5810fe5d44e9e29788b5f7e8d
Abbey Mondshein <5883999+abbeymondshein@users.noreply.github.com>
2020-08-25 14:13:30 -0400

    mechanics to add candles

--- altar.js
+++ altar.js
@@ -8,14 +8,40 @@ const CANDLE = " ┃";
 const DIVIDER = "◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆";
 const SHELF = "┍━━━━━━━━━━━━━━━━┑";
 
+const COLOR_OPTIONS = [
+  "black",
+  "red",
+  "green",
+  "yellow",
+  "blue",
+  "magenta",
+  "cyan",
+  "white",
+  "gray",
+  "redBright",
+  "greenBright",
+  "yellowBright",
+  "blueBright",
+  "magentaBright",
+  "cyanBright",
+  "whiteBright",
+];
+
+const APPROVED_COLORS = `OPTIONS: ${c.red("red")} ${c.green(
+  "green"
+)} ${c.yellow("yellow")} ${c.blue("blue")} ${c.magenta("magenta")} ${c.cyan(
+  "cyan"
+)} ${c.white("white")} ${c.grey("grey")}
+`;
+
 program
   .option("-l, --load", "Load Altar")
   .option("-j, --json", "Load from JSON File")
+  .option("-c, --candle <color>", "Add a candle")
   .description("Create and maintain an altar practice via the Command Line.")
   .parse(process.argv);
 
-console.log(`
-for options & help, enter "altar -h"
+console.log(`for options & help, enter "altar -h"
 
 ${DIVIDER}
 Welcome to Altar
@@ -23,11 +49,22 @@ ${DIVIDER}
 
 `);
 
-if (program.load) {
-  console.log("Loading altar");
-}
+const { load, candle } = program;
 
-if (program.json) {
+const verifyColor = (color) => {
+  return COLOR_OPTIONS.includes(color);
+};
+
+if (candle) {
+  if (!verifyColor(candle)) {
+    console.log(`The color you have chosen is not an approved option.`);
+    console.log(APPROVED_COLORS);
+    return;
+  }
+  const intention = program.args.join(" ");
+  console.log(`Adding ${candle} candle: `, intention);
+  //   console.log(intention);
+
   fs.readFile(`${__dirname}/altarItems.json`, "utf8", (err, data) => {
     if (err) {
       console.error(err);
@@ -35,16 +72,44 @@ if (program.json) {
     }
 
     // * Imports a sample set / starter via JSON
-    const candleEntries = JSON.parse(data);
+    const altarItems = JSON.parse(data);
+
+    const modifiedAltarItems = Object.assign([], altarItems);
+
+    modifiedAltarItems.push({
+      color: candle,
+      intention: intention,
+    });
+
+    fs.writeFile(
+      `${__dirname}/altarItems.json`,
+      JSON.stringify(modifiedAltarItems),
+      (err) => {
+        if (err) throw err;
+        console.log("candle added!");
+      }
+    );
+  });
+}
+
+if (load) {
+  fs.readFile(`${__dirname}/altarItems.json`, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    // * Imports a sample set / starter via JSON
+    const altarItems = JSON.parse(data);
 
     // todo extract as util
     const logFlames = (candleAmount) => {
       console.log(`${c.yellow(FLAME.repeat(candleAmount))}`);
     };
 
-    const logCandleWithColor = (candleEntries) => {
+    const logCandleWithColor = (altarItems) => {
       const combined = [];
-      candleEntries.forEach((element) => {
+      altarItems.forEach((element) => {
         const { color } = element;
         combined.push(`${c[color](CANDLE)}`);
       });
@@ -53,17 +118,17 @@ if (program.json) {
       return;
     };
 
-    const logCandleIntentions = (candleEntries) => {
-      candleEntries.forEach((candleEntry) => {
+    const logCandleIntentions = (altarItems) => {
+      altarItems.forEach((candleEntry) => {
         const { color, intention } = candleEntry;
         console.log(`${c[color](intention)}`);
       });
     };
 
     // * LOG CANDLE OUTPUT
-    logFlames(candleEntries.length);
-    logCandleWithColor(candleEntries);
+    logFlames(altarItems.length);
+    logCandleWithColor(altarItems);
     console.log(SHELF);
-    logCandleIntentions(candleEntries);
+    logCandleIntentions(altarItems);
   });
 }
