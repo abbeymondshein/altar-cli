4a4d5299a39827bd603c3f206d1db8863c3f0de3
Abbey Mondshein <5883999+abbeymondshein@users.noreply.github.com>
2020-08-25 11:13:05 -0400

    configure json command

--- altar.js
+++ altar.js
@@ -10,56 +10,60 @@ const SHELF = "┍━━━━━━━━━━━━━━━━┑";
 
 program
   .option("-l, --load", "Load Altar")
+  .option("-j, --json", "Load from JSON File")
   .description("Create and maintain an altar practice via the Command Line.")
   .parse(process.argv);
 
 console.log(`
+for options & help, enter "altar -h"
+
 ${DIVIDER}
 Welcome to Altar
 ${DIVIDER}
 
-for options & help, enter "altar -h"
 `);
 
 if (program.load) {
   console.log("Loading altar");
 }
 
-fs.readFile("./altarItems.json", "utf8", (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
+if (program.json) {
+  fs.readFile("./altarItems.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
 
-  // * Imports a sample set / starter via JSON
-  const candleEntries = JSON.parse(data);
+    // * Imports a sample set / starter via JSON
+    const candleEntries = JSON.parse(data);
 
-  // todo extract as util
-  const logFlames = (candleAmount) => {
-    console.log(`${c.yellow(FLAME.repeat(candleAmount))}`);
-  };
+    // todo extract as util
+    const logFlames = (candleAmount) => {
+      console.log(`${c.yellow(FLAME.repeat(candleAmount))}`);
+    };
 
-  const logCandleWithColor = (candleEntries) => {
-    const combined = [];
-    candleEntries.forEach((element) => {
-      const { color } = element;
-      combined.push(`${c[color](CANDLE)}`);
-    });
-    console.log(combined.join(""));
+    const logCandleWithColor = (candleEntries) => {
+      const combined = [];
+      candleEntries.forEach((element) => {
+        const { color } = element;
+        combined.push(`${c[color](CANDLE)}`);
+      });
+      console.log(combined.join(""));
 
-    return;
-  };
+      return;
+    };
 
-  const logCandleIntentions = (candleEntries) => {
-    candleEntries.forEach((candleEntry) => {
-      const { color, intention } = candleEntry;
-      console.log(`${c[color](intention)}`);
-    });
-  };
+    const logCandleIntentions = (candleEntries) => {
+      candleEntries.forEach((candleEntry) => {
+        const { color, intention } = candleEntry;
+        console.log(`${c[color](intention)}`);
+      });
+    };
 
-  // * LOG CANDLE OUTPUT
-  logFlames(candleEntries.length);
-  logCandleWithColor(candleEntries);
-  console.log(SHELF);
-  logCandleIntentions(candleEntries);
-});
+    // * LOG CANDLE OUTPUT
+    logFlames(candleEntries.length);
+    logCandleWithColor(candleEntries);
+    console.log(SHELF);
+    logCandleIntentions(candleEntries);
+  });
+}
